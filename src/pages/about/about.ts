178f87a9adb2b6510f4d688170806be395286ae3
UdithaGunaratne <gratne.16@cse.mrt.ac.lk>
2018-10-06 21:21:34 +0530

    Http Post Added

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +8,24 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  selectedFile: File = null;
 
+  constructor(public navCtrl: NavController, private http: HttpClient) {
+
+  }
+
+  onFileSelected(event){
+    console.log(event);
+    this.selectedFile = event.target.files[0];
+  }
+
+  onUpload(){
+    const fd = new FormData();
+    fd.append('image', this.selectedFile, this.selectedFile.name); 
+    this.http.post('https://us-central1-project-1-a5d90.cloudfunctions.net/saveImage', fd) //binary data vlata, fd = this.select
+      .subscribe(res=>{
+        console.log(res);
+      });
   }
 
 }
